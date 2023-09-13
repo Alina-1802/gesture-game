c9b4b94d1f6a5b53e23fc54526422e028e6e63da
Alina Stec <81440573+Alina-1802@users.noreply.github.com>
2023-09-13 12:06:54 +0200

    Updated CameraController

--- Gesture-game/Assets/Scripts/CameraController.cs
+++ Gesture-game/Assets/Scripts/CameraController.cs
@@ -3,26 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Scripting.Python;
 using UnityEditor;
-
+using System;
 //using Python.Runtime;
-//using Python.Runtime;
-
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+/*        PythonRunner.RunString(@"
+            import UnityEngine;
+            UnityEngine.Debug.Log('hello world')
+        ");*/
 
-        PythonRunner.RunString(@"
-        import UnityEngine;
-        UnityEngine.Debug.Log('hello world')
-        ");
-        PythonRunner.RunFile("Assets\\Scripts\\face_position_recognition.py");
-}
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        PythonRunner.RunFile("Assets\\Scripts\\face_position_recognition.py");
     }
 }
